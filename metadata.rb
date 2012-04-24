cbe8e625d94e39aab4de75d9607af971f85db4b6
Peter Donald <peter@realityforge.org>
2012-04-24 15:56:06 +1000

    Indicate the supported operating systems

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures authbind and defines resources for managing authorization"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
