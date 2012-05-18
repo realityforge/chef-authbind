a50dd335c78724ceeb339e469e1ff98e1706e509
Peter Donald <peter@realityforge.org>
2012-05-18 11:14:22 +1000

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "peter@realityforge.org"
 license          "Apache 2.0"
 description      "Installs/Configures authbind and defines resources for managing authorization"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 %w{ ubuntu debian }.each do |os|
   supports os
