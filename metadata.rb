245580939da9352310a576279dd96834e5fcfd88
Peter Donald <peter@realityforge.org>
2012-12-31 12:59:22 +1100

    Replace " quotes with '

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name             "authbind"
-maintainer       "Peter Donald"
-maintainer_email "peter@realityforge.org"
-license          "Apache 2.0"
-description      "Installs/Configures authbind and defines resources for managing authorization"
+name             'authbind'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs/Configures authbind and defines resources for managing authorization'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          '0.1.3'
 
 %w{ ubuntu debian }.each do |os|
   supports os
 end
 
-recipe "authbind::default", "Installs and configures Authbind"
+recipe 'authbind::default', 'Installs and configures Authbind'
